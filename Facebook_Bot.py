ac9f15aeff544cb555643fd470017a15b6121b7c
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2022-11-24 10:22:29 +0300

    read from gspread update

--- Facebook_Bot.py
+++ Facebook_Bot.py
@@ -12,7 +12,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 console = Console()
 
@@ -36,6 +37,23 @@ console.print("(5)  for   0%    accounts",
 my_comment = input("Please Enter for Comment: ")
 my_share = input("Please Enter for Share: ")
 
+
+console.print("0 for read account from local excel",
+              justify="center", style="white on black")
+console.print("1 for read account from google-sheet",
+              justify="center", style="white on black")
+
+read_excel_from = input("Please Enter account source: ")
+
+
+console.print("0 for read link from local",
+              justify="center", style="white on black")
+console.print("1 for read link from Telegram-BOT",
+              justify="center", style="white on black")
+
+
+read_link_from = input("Please Enter link source: ")
+
 home_url = 'https://www.facebook.com/'
 disbled_link = 'checkpoint/disabled/?next'
 password_fail = 'login/?privacy_mutation_token'
@@ -45,14 +63,33 @@ class social_media():
 
     def login(self):
         delay = random.randint(5, 20)
+
+        if read_excel_from == "0":
+            self.wb_acc = pd.read_excel("./accounts and comment/accounts.xlsx")
+
+        elif read_excel_from == "1":
+
+            self.scopes = [
+
+                'https://www.googleapis.com/auth/spreadsheets',
+                'https://www.googleapis.com/auth/drive',
+            ]
+            self.creds = ServiceAccountCredentials.from_json_keyfile_name(
+                "./key.json", scopes=self.scopes)
+            self.files = gspread.authorize(self.creds)
+            self.workbook = self.files.open("Account_Mgt")
+            self.sheet = self.workbook.worksheet('Sheet2')
+            self.wb_acc = pd.DataFrame(self.sheet.get_all_records())
+            print(self.wb_acc)
+        else:
+            print("chices are only 0 and 1")
+
+        self.user_name = self.sheet.col_values(1)
+        self.account_password = self.sheet.col_values(2)
+
         self.track_comment = 0
         self.track_share = 0
-
-        self.wb_acc = pd.read_excel("./accounts and comment/accounts.xlsx")
-        self.name = self.wb_acc['user_name'].tolist()
-        self.password = self.wb_acc['account_password'].tolist()
-
-        self.looper = len(self.name)
+        self.looper = len(self.user_name)
 
         self.wb_com = pd.read_excel("./accounts and comment/comments.xlsx")
         self.comment_list = self.wb_com['comments'].tolist()
@@ -81,8 +118,8 @@ class social_media():
                     console.print("Current account state ===> ",  self.i, "Account ", "  Last account ===>  ",
                                   self.looper, "Account ", justify="center", style="white on magenta")
 
-                    self.username = self.name[self.i]
-                    self.password = self.password[self.i]
+                    self.username = self.user_name[self.i]
+                    self.password = self.account_password[self.i]
                     self.comment = self.comment_list[self.i]
 
                     chrome_options = webdriver.ChromeOptions()
@@ -395,7 +432,12 @@ class social_media():
             time.sleep(delay)
 
     def target_link_perform(self):
-        try:
+        if read_link_from == "0":
+            with open("./links/link.txt", 'r+') as f:
+                self.link = f.readline()
+                print(self.link)
+
+        elif read_link_from == "1":
             delay = random.randint(5, 9)
             url = "https://api.telegram.org/bot5692464682:AAEKqvCKMsOKk2Po9m-dQP4_koR3OqumDjc/getUpdates?offset=-1"
 
@@ -415,10 +457,9 @@ class social_media():
                 self.link = data['result'][0]['edited_message']['text']
             except:
                 pass
-        except:
-            with open("./links/link.txt", 'r') as f:
-                self.link = f.readline()
-                print(self.link)
+
+        else:
+            print("chices are only 0 and 1")
 
         console.print("Get Link From Bot :  " + self.link,
                       style="link green" + self.link)
